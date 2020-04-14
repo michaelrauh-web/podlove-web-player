025be7a8af841b7edcf479eb0390c1cbcfbc177c
Alexander Heimbuch <alexander-heimbuch@users.noreply.github.com>
2020-04-14 19:11:24 +0200

    github-pages publish.

--- 5.1.1/player/8.js
+++ 5.1.1/player/8.js
@@ -1,3 +1,3 @@
-// [AIV_SHORT]  Build version: 5.1.0 - Monday, April 13th, 2020, 3:10:25 PM  
+// [AIV_SHORT]  Build version: 5.1.1 - Tuesday, April 14th, 2020, 5:10:27 PM  
  (window.webpackJsonp=window.webpackJsonp||[]).push([[8],{241:function(t,n,u){"use strict";u.r(n),n.default=u(220).default},37:function(t,n,u){"use strict";n.a=u(215).default}}]);
 //# sourceMappingURL=8.js.map 
